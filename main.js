dbae0a5503455ba1a6ae036cef3d728df97b0c2c
Ramon <ramonrpro@gmail.com>
2023-11-04 09:26:41 -0300

    Add init check

--- main.js
+++ main.js
@@ -4,20 +4,33 @@ form.addEventListener("submit", function (e) {
    const peso = form.querySelector("#peso").value;
    const altura = form.querySelector("#altura").value;
    const resultado = document.querySelector("#resultado");
-   let imc = peso / (((altura / 100) * altura) / 100);
+   
+   function setResult(msg) {
+      resultado.textContent = msg
+   }
+
+   if (!peso) {
+      setResult("Peso inválido");
+      return;
+   }
+   if (!altura) {
+      setResult("Altura inválida");
+      return;
+   }
+   let imc = peso / ((altura /100 )** 2 );
 
    imc = imc.toFixed(2);
    if (imc < 18.5) {
-      resultado.textContent = `Seu IMC é ${imc} você está abaixo do peso`;
+      setResult(`Seu IMC é ${imc} você está abaixo do peso`);
    } else if (imc > 18.5 && imc < 24.8) {
-      resultado.textContent = `Seu IMC é ${imc} você está no peso ideal.`;
+      setResult(`Seu IMC é ${imc} você está no peso ideal.`);
    } else if (imc > 25 && imc < 29.5) {
-      resultado.textContent = `Seu imc é ${imc} você está com sobrepeso`;
+      setResult(`Seu imc é ${imc} você está com sobrepeso`);
    } else if (imc > 30 && imc < 34.9) {
-      resultado.textContent = `Seu imc é ${imc} você está com obesidade grau I`;
+      setResult(`Seu imc é ${imc} você está com obesidade grau I`);
    } else if (imc > 35 && imc < 39.9) {
-      resultado.textContent = `Seu imc é ${imc} você está com obesidade grau II`;
+      setResult(`Seu imc é ${imc} você está com obesidade grau II`);
    } else if (imc > 40) {
-      resultado.textContent = `Seu imc é ${imc} você está com obesidade grau III`;
+      setResult(`Seu imc é ${imc} você está com obesidade grau III`);
    }
 });
